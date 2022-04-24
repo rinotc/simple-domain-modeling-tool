6f36d4e7b9b5bf6eef439ffc71bdef693ada60d4
toshinori.chiba <toshinori.chiba.p@gmail.com>
2022-04-24 19:20:58 +0900

    add ProjectOverviewTest

---
+++ sdmt-core/src/test/scala/dev/tchiba/sdmt/core/models/project/ProjectOverviewTest.scala
@@ -0,0 +1,56 @@
+package dev.tchiba.sdmt.core.models.project
+
+import dev.tchiba.sdmt.test.BaseTest
+
+class ProjectOverviewTest extends BaseTest {
+
+  "ProjectOverview Requirement" when {
+    "501 length value" should {
+      "throw requirement error" in {
+        val value = "A" * 501
+        value.length shouldBe 501
+        assertThrows[IllegalArgumentException] {
+          ProjectOverview(value)
+        }
+      }
+    }
+  }
+
+  "equals" when {
+    "compare same underlying value instance" should {
+      "return true" in {
+        val overviewA = ProjectOverview("ABC")
+        val overviewB = ProjectOverview("ABC")
+
+        overviewA shouldEqual overviewB
+      }
+    }
+
+    "compare not same underlying value instance" should {
+      "return false" in {
+        val overviewA = ProjectOverview("ABC")
+        val overviewB = ProjectOverview("XYZ")
+
+        overviewA should not equal overviewB
+      }
+    }
+  }
+
+  "validate" when {
+    "501 length String value" should {
+      "return left" in {
+        val value  = "A" * 501
+        val actual = ProjectOverview.validate(value)
+        actual shouldBe Symbol("left")
+      }
+    }
+
+    "empty String value" should {
+      "return right" in {
+        val actual = ProjectOverview.validate("")
+        actual shouldBe Symbol("right")
+        actual.value.value shouldBe ""
+      }
+    }
+  }
+}
