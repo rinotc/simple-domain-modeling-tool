a3a2e2e9c03224c75517978c81ac3a4110394b43
toshinori.chiba <toshinori.chiba.p@gmail.com>
2022-04-25 23:18:39 +0900

    add validate method to project-id

--- front/angular-front/src/app/models/project/id/project-id.spec.ts
+++ front/angular-front/src/app/models/project/id/project-id.spec.ts
@@ -1,5 +1,6 @@
-import { ProjectId } from './project-id';
+import {ProjectId} from './project-id';
 import {expect} from "@angular/flex-layout/_private-utils/testing";
+import * as E from "fp-ts/Either";
 
 describe('ProjectId', () => {
   it('should create an instance', () => {
@@ -8,9 +9,22 @@ describe('ProjectId', () => {
     expect(new ProjectId(value)).toBeTruthy();
   });
 
-  it('should throw error when value is 35.', () => {
+  it('should throw error when value length is 35.', () => {
     const value = 'abcde-fghij-klmno-pqrst-uvwxy-z1234'
     expect(value.length).toBe(35);
     expect(() => new ProjectId(value)).toThrow();
   });
+
+  it('should return Left when value length is 35.', () => {
+    const value = 'abcde-fghij-klmno-pqrst-uvwxy-z1234'
+    expect(value.length).toBe(35);
+    const actual = ProjectId.validate(value);
+    expect(E.isLeft(actual)).toBe(true)
+  });
+
+  it('should return right when value length is 36', () => {
+    const value = 'abcde-fghij-klmno-pqrst-uvwxy-z12345'
+    expect(value.length).toBe(36);
+    expect(ProjectId.validate(value)).toEqual(E.right(new ProjectId(value)));
+  })
 });
--- front/angular-front/src/app/models/project/id/project-id.ts
+++ front/angular-front/src/app/models/project/id/project-id.ts
@@ -1,4 +1,5 @@
 import {PreferNominal} from "../../prefer-nominal";
+import * as E from "fp-ts/Either";
 
 // noinspection JSUnusedLocalSymbols,JSUnusedGlobalSymbols
 export class ProjectId {
@@ -6,10 +7,23 @@ export class ProjectId {
   readonly value: string
 
   constructor(value: string) {
-    const require = value.length === 36;
-    if (!require) {
-      throw new TypeError('project id value length must be 36 length.')
+    if (!ProjectId.mustValueLengthEqual36(value)) {
+      throw new TypeError(ProjectId.requirementErrorMessage);
     }
     this.value = value;
+  }
+
+  static validate(value: string): E.Either<string, ProjectId> {
+    if (this.mustValueLengthEqual36(value)) {
+      const projectId = new ProjectId(value)
+      return E.right(projectId)
+    }
+    return E.left(this.requirementErrorMessage);
+  }
+
+  private static requirementErrorMessage: string = 'project id value length must be 36 length.';
+
+  private static mustValueLengthEqual36(value: string): boolean {
+    return value.length === 36;
   }
 }
