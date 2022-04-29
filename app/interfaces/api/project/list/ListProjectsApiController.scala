3913ef83d80cd8d977feae3937d53b8a4d4d78d4
toshinori.chiba <toshinori.chiba.p@gmail.com>
2022-04-29 23:10:47 +0900

    remove unused circe

--- app/interfaces/api/project/list/ListProjectsApiController.scala
+++ app/interfaces/api/project/list/ListProjectsApiController.scala
@@ -3,7 +3,6 @@ package interfaces.api.project.list
 import dev.tchiba.sdmt.core.models.project.ProjectRepository
 import interfaces.api.project.json.ProjectResponse
 import interfaces.json.CollectionResponse
-import play.api.libs.circe.Circe
 import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
 
 import javax.inject.Inject
@@ -11,8 +10,7 @@ import javax.inject.Inject
 final class ListProjectsApiController @Inject() (
     cc: ControllerComponents,
     projectRepository: ProjectRepository
-) extends AbstractController(cc)
-    with Circe {
+) extends AbstractController(cc) {
 
   def action(): Action[AnyContent] = Action {
     val projects = projectRepository.all
