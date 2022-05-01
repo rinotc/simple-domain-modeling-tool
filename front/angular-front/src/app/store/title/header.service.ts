39a8757001cd49cf3eb2748033753c4800f52bc8
toshinori.chiba <toshinori.chiba.p@gmail.com>
2022-05-01 23:29:56 +0900

    commit

---
+++ front/angular-front/src/app/store/title/header.service.ts
@@ -0,0 +1,12 @@
+import {Injectable} from '@angular/core';
+import {HeaderStore} from './header.store';
+
+@Injectable({ providedIn: 'root' })
+export class HeaderService {
+
+  constructor(private titleStore: HeaderStore) {}
+
+  update(title: string) {
+    this.titleStore.update({ context: title });
+  }
+}
