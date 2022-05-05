72143c8cfe3b5c9716c602688ed3d0467d4fd68e
toshinori.chiba <toshinori.chiba.p@gmail.com>
2022-05-05 14:53:47 +0900

    fix using test

--- test/interfaces/api/domainmodel/create/CreateDomainModelRequestTest.scala
+++ test/interfaces/api/domainmodel/create/CreateDomainModelRequestTest.scala
@@ -1,10 +1,10 @@
 package interfaces.api.domainmodel.create
 
 import dev.tchiba.sdmt.core.domainmodel.{EnglishName, JapaneseName}
-import dev.tchiba.sdmt.test.BaseTest
 import interfaces.json.RequestValidationError
+import org.scalatestplus.play.PlaySpec
 
-class CreateDomainModelRequestTest extends BaseTest {
+class CreateDomainModelRequestTest extends PlaySpec {
 
   "primary constructor" when {
     "invalid japaneseName request" should {
