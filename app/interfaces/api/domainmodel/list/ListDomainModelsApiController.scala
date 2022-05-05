743afe46dd99b30a9c67108640b90affcd66d64d
toshinori.chiba <toshinori.chiba.p@gmail.com>
2022-05-05 15:04:24 +0900

    create list domain models api

---
+++ app/interfaces/api/domainmodel/list/ListDomainModelsApiController.scala
@@ -0,0 +1,35 @@
+package interfaces.api.domainmodel.list
+
+import dev.tchiba.sdmt.core.boundedContext.BoundedContextId
+import dev.tchiba.sdmt.core.domainmodel.DomainModelRepository
+import interfaces.api.QueryValidator
+import interfaces.api.domainmodel.json.DomainModelResponse
+import interfaces.json.CollectionResponse
+import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
+
+import javax.inject.Inject
+import scala.concurrent.ExecutionContext
+
+class ListDomainModelsApiController @Inject() (
+    cc: ControllerComponents,
+    domainModelRepository: DomainModelRepository
+)(implicit ec: ExecutionContext)
+    extends AbstractController(cc) {
+
+  /**
+   * 境界づけられたコンテキスト一覧API
+   *
+   * @param bcId 境界づけられたコンテキストID
+   * @return 境界づけらえれたコンテキスト一覧
+   */
+  def action(bcId: String): Action[AnyContent] = Action {
+    QueryValidator.sync {
+      BoundedContextId.fromString(bcId)
+    } { boundedContextId =>
+      val domainModels = domainModelRepository.listBy(boundedContextId)
+      val jsons        = domainModels.map(DomainModelResponse.apply).map(_.json)
+      val response     = CollectionResponse(jsons)
+      Ok(response.json)
+    }
+  }
+}
