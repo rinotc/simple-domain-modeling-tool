195dfe72b8a22b407317ec12631dd3bef2394aa1
toshinori.chiba <toshinori.chiba.p@gmail.com>
2022-05-10 00:25:35 +0900

    fix

--- app/interfaces/api/domainmodel/list/ListDomainModelsApiController.scala
+++ app/interfaces/api/domainmodel/list/ListDomainModelsApiController.scala
@@ -23,7 +23,7 @@ class ListDomainModelsApiController @Inject() (
    */
   def action(bcId: String): Action[AnyContent] = Action {
     QueryValidator.sync {
-      BoundedContextId.fromString(bcId)
+      BoundedContextId.validate(bcId)
     } { boundedContextId =>
       val domainModels = domainModelRepository.listBy(boundedContextId)
       val jsons        = domainModels.map(DomainModelResponse.apply).map(_.json)
