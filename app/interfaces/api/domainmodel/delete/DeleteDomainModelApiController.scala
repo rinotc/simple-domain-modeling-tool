1074478ab6a0c3d3560ce4056b6edd8f48e87c35
toshinori.chiba <toshinori.chiba.p@gmail.com>
2022-05-23 00:13:21 +0900

    ドメインモデル削除APIの作成

---
+++ app/interfaces/api/domainmodel/delete/DeleteDomainModelApiController.scala
@@ -0,0 +1,28 @@
+package interfaces.api.domainmodel.delete
+
+import dev.tchiba.sdmt.core.domainmodel.{DomainModelId, DomainModelRepository}
+import interfaces.api.QueryValidator
+import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
+
+import javax.inject.Inject
+
+class DeleteDomainModelApiController @Inject() (
+    cc: ControllerComponents,
+    domainModelRepository: DomainModelRepository
+) extends AbstractController(cc) {
+
+  /**
+   * 指定したIDのドメインモデルを削除するAPI
+   *
+   * @param dmId ドメインモデルID文字列
+   * @return [[NoContent]]
+   */
+  def action(bcId: String, dmId: String): Action[AnyContent] = Action {
+    QueryValidator.sync {
+      DomainModelId.validate(dmId)
+    } { domainModelId =>
+      domainModelRepository.delete(domainModelId)
+      NoContent
+    }
+  }
+}
