00c170f5a4320479700fd698f36e2c4b3e637e1d
toshinori.chiba <toshinori.chiba.p@gmail.com>
2022-05-22 22:41:38 +0900

    画面作成

--- front/angular-front/src/app/views/pages/domainModel/create/create-domain-model-page.component.ts
+++ front/angular-front/src/app/views/pages/domainModel/create/create-domain-model-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+import {UbiquitousName} from "../../../../models/domainModel/ubiquitousName/ubiquitous-name";
+import {requirement} from "../../../../dbc/dbc";
+import {EnglishName} from "../../../../models/domainModel/englishName/english-name";
+import {Knowledge} from "../../../../models/domainModel/knowledge/knowledge";
 
 @Component({
   selector: 'app-create-domain-model',
@@ -7,9 +12,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateDomainModelPageComponent implements OnInit {
 
-  constructor() { }
+  inputUbiquitousName: string = '';
+  inputEnglishName: string = '';
+  inputKnowledge: string = '';
+
+  constructor(
+    router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  get hasUbiquitousNameValidationError(): boolean {
+    return !UbiquitousName.isValid(this.inputUbiquitousName);
+  }
+
+  get ubiquitousNameValidationErrorMessage(): string {
+    requirement(this.hasUbiquitousNameValidationError, 'this method must only call input ubiquitous name has validation error.');
+    return UbiquitousName.validationErrorMessage;
+  }
+
+  get hasEnglishNameValidationError(): boolean {
+    return !EnglishName.isValid(this.inputEnglishName);
+  }
+
+  get englishNameValidationErrorMessage(): string {
+    requirement(this.hasEnglishNameValidationError, 'this method must only call input english name has validation error');
+    return EnglishName.validationErrorMessage;
+  }
+
+  get canSubmit(): boolean {
+    return UbiquitousName.isValid(this.inputUbiquitousName) && EnglishName.isValid(this.inputEnglishName);
+  }
+
+  onSubmit(): void {
+    requirement(this.canSubmit, 'submit button only can push all validation were passed.');
+    const ubiquitousName = new UbiquitousName(this.inputUbiquitousName);
+    const englishName = new EnglishName(this.inputEnglishName);
+    const knowledge = new Knowledge(this.inputKnowledge);
+
+    console.log('submit');
+  }
 }
