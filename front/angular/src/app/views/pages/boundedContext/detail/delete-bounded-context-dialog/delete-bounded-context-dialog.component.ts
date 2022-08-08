8b2899e9200b267cb7cb9b79c57b3ec9fb4c7c51
toshinori.chiba <toshinori.chiba.p@gmail.com>
2022-08-09 01:04:22 +0900

    削除できる一歩手前まで

---
+++ front/angular/src/app/views/pages/boundedContext/detail/delete-bounded-context-dialog/delete-bounded-context-dialog.component.ts
@@ -0,0 +1,37 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { BoundedContextDetailPageComponent } from '../bounded-context-detail-page.component';
+import { requirement } from '../../../../../dbc/dbc';
+
+export interface DialogInput {
+  boundedContextName: string;
+  boundedContextAlias: string;
+}
+
+@Component({
+  selector: 'app-delete-bounded-context-dialog',
+  templateUrl: './delete-bounded-context-dialog.component.html',
+  styleUrls: ['./delete-bounded-context-dialog.component.scss'],
+})
+export class DeleteBoundedContextDialogComponent implements OnInit {
+  inputBoundedContextAlias: string = '';
+  constructor(
+    public dialogRef: MatDialogRef<BoundedContextDetailPageComponent>,
+    @Inject(MAT_DIALOG_DATA) public input: DialogInput
+  ) {}
+
+  ngOnInit(): void {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  get canDelete(): boolean {
+    return this.inputBoundedContextAlias === this.input.boundedContextAlias;
+  }
+
+  onClickDelete() {
+    requirement(this.canDelete, 'can delete onlysame alias input.');
+    console.log('削除');
+  }
+}
