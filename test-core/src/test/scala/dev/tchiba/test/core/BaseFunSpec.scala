c0102ca9cae619275c55946a413c1174641d0e31
toshinori.chiba <toshinori.chiba.p@gmail.com>
2022-08-12 16:00:50 +0900

    add BaseFunSpec

---
+++ test-core/src/test/scala/dev/tchiba/test/core/BaseFunSpec.scala
@@ -0,0 +1,17 @@
+package dev.tchiba.test.core
+
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.{EitherValues, Inside, OptionValues}
+
+trait BaseFunSpec extends AnyFunSpec with Matchers with OptionValues with EitherValues with Inside {
+  implicit protected class ValueTestContextOps[A](value: A) {
+    def some: Option[A] = Some(value)
+
+    def left[R]: Either[A, R] = Left(value)
+
+    def right[L]: Either[L, A] = Right(value)
+  }
+
+  final val unit: Unit = ()
+}
