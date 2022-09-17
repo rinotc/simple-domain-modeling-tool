43bab25a0254aabcec175b7eddc4eaf8e6518015
toshinori.chiba <toshinori.chiba.p@gmail.com>
2022-09-18 00:03:14 +0900

    add comment

--- sdmt-usecase/src/main/scala/dev/tchiba/sdmt/usecase/boundedContext/list/ListBoundedContextsUseCase.scala
+++ sdmt-usecase/src/main/scala/dev/tchiba/sdmt/usecase/boundedContext/list/ListBoundedContextsUseCase.scala
@@ -2,4 +2,7 @@ package dev.tchiba.sdmt.usecase.boundedContext.list
 
 import dev.tchiba.arch.usecase.{NoInput, UseCase}
 
+/**
+ * 境界づけられたコンテキストを一覧するユースケース
+ */
 abstract class ListBoundedContextsUseCase extends UseCase[NoInput[ListBoundedContextsOutput], ListBoundedContextsOutput]
