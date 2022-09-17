e2044b9d2db1d7b506603a9ba962ee3e0886fa0d
toshinori.chiba <toshinori.chiba.p@gmail.com>
2022-09-18 00:02:38 +0900

    add NoInput

--- app/interfaces/api/boundedContext/list/ListBoundedContextsApiController.scala
+++ app/interfaces/api/boundedContext/list/ListBoundedContextsApiController.scala
@@ -1,6 +1,7 @@
 package interfaces.api.boundedContext.list
 
-import dev.tchiba.sdmt.core.boundedContext.BoundedContextRepository
+import dev.tchiba.arch.usecase.NoInput
+import dev.tchiba.sdmt.usecase.boundedContext.list.{ListBoundedContextsOutput, ListBoundedContextsUseCase}
 import interfaces.api.boundedContext.json.BoundedContextResponse
 import interfaces.json.CollectionResponse
 import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
@@ -9,11 +10,11 @@ import javax.inject.Inject
 
 final class ListBoundedContextsApiController @Inject() (
     cc: ControllerComponents,
-    boundedContextRepository: BoundedContextRepository
+    boundedContextsUseCase: ListBoundedContextsUseCase
 ) extends AbstractController(cc) {
 
   def action(): Action[AnyContent] = Action {
-    val boundedContexts = boundedContextRepository.all
+    val boundedContexts = boundedContextsUseCase.handle(NoInput[ListBoundedContextsOutput]).boundedContexts
     val jsons           = boundedContexts.map(BoundedContextResponse.apply).map(_.json)
     val response        = CollectionResponse(jsons)
     Ok(response.json)
---
+++ sdmt-usecase/src/main/scala/dev/tchiba/sdmt/usecase/boundedContext/list/ListBoundedContextsUseCase.scala
@@ -0,0 +1,5 @@
+package dev.tchiba.sdmt.usecase.boundedContext.list
+
+import dev.tchiba.arch.usecase.{NoInput, UseCase}
+
+abstract class ListBoundedContextsUseCase extends UseCase[NoInput[ListBoundedContextsOutput], ListBoundedContextsOutput]
